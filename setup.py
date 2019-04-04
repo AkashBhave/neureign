122dafc12e78a50cf3d9b1a3ec0719f261b54609
Akash Bhave <akashbbhave@gmail.com>
2019-04-04 08:16:00 -0400

    Create package structure, create .gitignore
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="neureign",
+    version="0.1",
+    description="Simple neural network library",
+    url="http://github.com/AkashBhave/neureign",
+    author="Akash Bhave",
+    license="MIT",
+    package=["neureign"]
+)
